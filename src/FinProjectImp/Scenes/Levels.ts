cf946511771d2b89429ec915cf3cd57721888d64
Aidan Foley <aidan.foley@stonybrook.edu>
2024-04-08 16:22:06 -0400

    add wormholes

--- src/FinProjectImp/Scenes/Levels.ts
+++ src/FinProjectImp/Scenes/Levels.ts
@@ -1,6 +1,6 @@
 import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
 import Viewport from "../../Wolfie2D/SceneGraph/Viewport";
-import Level, { Asteroid } from "./LevelType";
+import Level, { Asteroid, WormholePair } from "./LevelType";
 
 // bhp - Position of the black hole
 // cp - Position of the cue
@@ -21,15 +21,17 @@ export default class Levels {
 	static getLevel1(viewport:Viewport) : Level {
 		var bhp = new Vec2(150, viewport.getCenter().y)
 		var asteroids : Array<Asteroid> = [new Asteroid(viewport.getCenter().clone(), 10)]
+		var wormholePairs : Array<WormholePair> = [new WormholePair(new Vec2(600, 700), new Vec2(600, 200))]
 		var cp = new Vec2(viewport.getCenter().x+400, viewport.getCenter().y)
-		return new Level(bhp,asteroids,cp);
+		return new Level(bhp, asteroids, wormholePairs, cp);
 	}
 
 	static getLevel2(viewport:Viewport) : Level {
 		var bhp = new Vec2(150, viewport.getCenter().y)
 		var asteroids : Array<Asteroid> = [new Asteroid(new Vec2(viewport.getCenter().x, viewport.getCenter().y-200), 10),
 											new Asteroid(new Vec2(viewport.getCenter().x, viewport.getCenter().y-2100), 10)]
+		var wormholePairs : Array<WormholePair> = [new WormholePair(viewport.getCenter().clone(), new Vec2(30, 30))]
 		var cp = new Vec2(viewport.getCenter().x+400, viewport.getCenter().y)
-		return new Level(bhp,asteroids,cp);
+		return new Level(bhp, asteroids, wormholePairs, cp);
 	}
 }
